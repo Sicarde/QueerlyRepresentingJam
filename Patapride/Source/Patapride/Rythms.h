106cb36258b3eb62d070b4fc5919f0b77ed4a91a
makoto <makotogames@yopmail.com>
2018-05-10 13:44:22 +0200

    pause menu (ugly and no options nor level selection menu)

--- Patapride/Source/Patapride/Rythms.h
+++ Patapride/Source/Patapride/Rythms.h
@@ -5,16 +5,17 @@
 //button names are in Edit->Project Settings->Input
 int nbMSecPerf = 800;
 int nbMSecGood = 1200;
-TArray<FString> buttonsNames = { "Up", "Down", "Left", "Right", "Square", "Triangle", "Circle", "Cross" };
+TArray<FString> buttonsNames = { "Square", "Triangle", "Circle", "Cross" };
 
 struct Note {
 	int min;
 	int sec;
 	int msec;
 	FString button;
+	int duration;
 };
 
 TArray<Note> testLevel = {
-	{ 0, 2, 0, "Up" },
-	{ 0, 7, 0, "Down" }
+	{ 0, 2, 0, "Square", 0 },
+	{ 0, 7, 0, "Cross", 800 }
 };
